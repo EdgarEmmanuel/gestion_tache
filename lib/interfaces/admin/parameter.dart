ca769080c4683d6871bf227cd5bad32110494e21
Edgar <mmnledgar@gmail.com>
2023-05-23 12:01:50 +0000

    working on pulling changes

--- lib/interfaces/admin/parameter.dart
+++ lib/interfaces/admin/parameter.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:gestion_tache/interfaces/admin/accueil_admin.dart';
 import 'package:gestion_tache/interfaces/auth/auth.dart';
 import 'package:gestion_tache/interfaces/auth/rememberMe.dart';
+import 'package:gestion_tache/globals/globals.dart' as globals;
 
 class Parameter extends StatefulWidget {
   const Parameter({super.key});
@@ -57,8 +58,13 @@ class _Parameter extends State<Parameter> {
           ],
         ),
         body: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            SwitchExample(),
+            Padding(
+              padding: EdgeInsets.all(10.0),
+              child: SwitchExample(),
+            )
           ],
         ),
       ),
@@ -75,19 +81,39 @@ class SwitchExample extends StatefulWidget {
 
 class _SwitchExampleState extends State<SwitchExample> {
   bool light = true;
+  static const SERVEUR_DISTANT = "Serveur Distant";
+  static const FIREBASE_SERVEUR = "Serveur Firebase";
+  String dataSource = SERVEUR_DISTANT;
 
   @override
   Widget build(BuildContext context) {
-    return Switch(
-      // This bool value toggles the switch.
-      value: light,
-      activeColor: Colors.red,
-      onChanged: (bool value) {
-        // This is called when the user toggles the switch.
-        setState(() {
-          light = value;
-        });
-      },
+    return Column(
+      children: [
+        Text(
+          "Source de Donnees",
+          textAlign: TextAlign.center,
+        ),
+        SizedBox(height: 30.0),
+        Switch(
+          // This bool value toggles the switch.
+          value: light,
+          activeColor: Colors.red,
+          onChanged: (bool value) {
+            // This is called when the user toggles the switch.
+            setState(() {
+              light = value;
+              if(dataSource == SERVEUR_DISTANT){
+                dataSource = FIREBASE_SERVEUR ;
+                globals.isFirebase = true ;
+              } else {
+                dataSource = SERVEUR_DISTANT ;
+                globals.isFirebase = false ;
+              }
+            });
+          },
+        ),
+        Text("${dataSource}")
+      ],
     );
   }
 }
